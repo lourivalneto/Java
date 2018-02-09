ca5c350502a4c2871bc01ff34136752b2b065f19
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-02-09 20:09:00 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Exer06 {
 
+    int contJogador1 = 0;
+    int contJogador2 = 0;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String nomeJogador1 = "";
@@ -17,26 +20,28 @@ public class Exer06 {
 
         int linhaJogador1 = 0;
         int colunaJogador1 = 0;
-        
+
         int linhaJogador2 = 0;
         int colunaJogador2 = 0;
+
+        boolean resposta = false;
 
         int[][] jogoDaVelha = new int[3][3];
 
         System.out.println(
-            "\tCOLUNA 1 COLUNA 2 COLUNA 3\n" +
-            "\n\t\t|\t|\n" +
-            "LINHA 1 \t|\t|\n" +
-            "\t\t|\t|\n" +
-            "\t_ _ _ __|_ _ _ _|__ _ _ _\n" +
-            "\t\t|\t|\n" +
-            "LINHA 2\t\t|\t|\n" +
-            "\t\t|\t|\n" +
-            "\t_ _ _ __|_ _ _ _|__ _ _ _\n" +
-            "\t\t|\t|\n" +
-            "\t\t|\t|\n" +
-            "LINHA 3\t\t|\t|\n" +
-            "\t\t|\t|\n");
+                "\tCOLUNA 1 COLUNA 2 COLUNA 3\n"
+                + "\n\t\t|\t|\n"
+                + "LINHA 1 \t|\t|\n"
+                + "\t\t|\t|\n"
+                + "\t_ _ _ __|_ _ _ _|__ _ _ _\n"
+                + "\t\t|\t|\n"
+                + "LINHA 2\t\t|\t|\n"
+                + "\t\t|\t|\n"
+                + "\t_ _ _ __|_ _ _ _|__ _ _ _\n"
+                + "\t\t|\t|\n"
+                + "\t\t|\t|\n"
+                + "LINHA 3\t\t|\t|\n"
+                + "\t\t|\t|\n");
 
         for (int i = 0; i < jogoDaVelha.length; i++) {
             for (int j = 0; j < jogoDaVelha[i].length; j++) {
@@ -51,7 +56,7 @@ public class Exer06 {
                         do {
                             System.out.println("Opção(ões) inválida(s). Tente novamente.");
                             System.out.println("\t" + nomeJogador1 + ", em que posição "
-                            + "você irá marcar a bolinha(O)?: ");
+                                    + "você irá marcar a bolinha(O)?: ");
                             System.out.println("Linha: ");
                             linhaJogador1 = scan.nextInt();
                             System.out.println("Coluna: ");
@@ -59,26 +64,31 @@ public class Exer06 {
 
                         } while ((linhaJogador1 < 1 || linhaJogador1 > 3) || (colunaJogador1 < 1 || colunaJogador1 > 3));
                     }
-                    
-                    if( jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] == 0 ) {
-                       jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] = -1; 
+
+                    if (jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] == 0) {
+                        jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] = -1;
                     } else { // EVITANDO PREENCHIMENTO EM POSIÇÃO JÁ PREENCHIDA
-                            do {
+                        do {
                             System.out.println("Essa posição já está marcada. Tente outra posição.");
-                                do {
+                            do {
                                 System.out.println("Opção(ões) inválida(s). Tente novamente.");
                                 System.out.println("\t" + nomeJogador1 + ", em que posição "
-                                + "você irá marcar a bolinha(O)?: ");
+                                        + "você irá marcar a bolinha(O)?: ");
                                 System.out.println("Linha: ");
                                 linhaJogador1 = scan.nextInt();
                                 System.out.println("Coluna: ");
                                 colunaJogador1 = scan.nextInt();
 
-                                } while ((linhaJogador1 < 1 || linhaJogador1 > 3) || (colunaJogador1 < 1 || colunaJogador1 > 3));
-                            } while( jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] != 0 );
-                            jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] = -1; 
+                            } while ((linhaJogador1 < 1 || linhaJogador1 > 3) || (colunaJogador1 < 1 || colunaJogador1 > 3));
+                        } while (jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] != 0);
+                        jogoDaVelha[linhaJogador1 - 1][colunaJogador1 - 1] = -1;
+                        resposta = verificaGanhador1(jogoDaVelha);
+                        if (resposta) {
+                            System.out.println(nomeJogador1 + ", você é o grande ganhador deste Jogo.");
+                            break;
                         }
-                    
+                    }
+
                 } else if (j % 2 != 0) {
                     System.out.println("\t" + nomeJogador2 + ", em que posição "
                             + "você irá marcar o \"X\")?: ");
@@ -90,7 +100,7 @@ public class Exer06 {
                         do {
                             System.out.println("Opção(ões) inválida(s). Tente novamente.");
                             System.out.println("\t" + nomeJogador2 + ", em que posição "
-                            + "você irá marcar o \"X\"?: ");
+                                    + "você irá marcar o \"X\"?: ");
                             System.out.println("Linha: ");
                             linhaJogador2 = scan.nextInt();
                             System.out.println("Coluna: ");
@@ -98,28 +108,87 @@ public class Exer06 {
 
                         } while ((linhaJogador2 < 1 || linhaJogador2 > 3) || (colunaJogador2 < 1 || colunaJogador2 > 3));
                     }
-                    
-                    if(jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] == 0){
-                    jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] = 1;
+
+                    if (jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] == 0) {
+                        jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] = 1;
                     } else { // EVITANDO PREENCHIMENTO EM POSIÇÃO JÁ PREENCHIDA
                         do {
                             System.out.println("Essa posição já está marcada. Tente outra posição.");
-                                do {
+                            do {
                                 System.out.println("Opção(ões) inválida(s). Tente novamente.");
                                 System.out.println("\t" + nomeJogador2 + ", em que posição "
-                                + "você irá marcar o \"X\"?: ");
+                                        + "você irá marcar o \"X\"?: ");
                                 System.out.println("Linha: ");
                                 linhaJogador2 = scan.nextInt();
                                 System.out.println("Coluna: ");
                                 colunaJogador2 = scan.nextInt();
 
-                                } while ((linhaJogador2 < 1 || linhaJogador2 > 3) || (colunaJogador2 < 1 || colunaJogador2 > 3));
-                            } while( jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] != 0 );
-                            jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] = 1;
+                            } while ((linhaJogador2 < 1 || linhaJogador2 > 3) || (colunaJogador2 < 1 || colunaJogador2 > 3));
+                        } while (jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] != 0);
+                        jogoDaVelha[linhaJogador2 - 1][colunaJogador2 - 1] = 1;
+                        resposta = verificaGanhador1(jogoDaVelha);
+                        if (resposta) {
+                            System.out.println(nomeJogador2 + ", você é o grande ganhador deste Jogo.");
+                            break;
+                        }
                     }
                 }
             }
         }
+    }
+
+    public static boolean verificaGanhador1(int[][] array) {
+        
+        boolean resposta = false;
+        
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[0][0] + array[0][1] + array[0][2] == -3) {
+                    resposta = true;
+
+                } else if (array[1][0] + array[1][1] + array[1][2] == -3) {
+                    resposta = true;
+                } else if (array[2][0] + array[2][1] + array[2][2] == -3) {
+                    resposta = true;
+                } else if (array[0][0] + array[1][1] + array[2][2] == -3) {
+                    resposta = true;
+                } else if (array[0][2] + array[1][1] + array[2][0] == -3) {
+                    resposta = true;
+                }
+
+            }
+
+        }
+        return resposta;
+
+    }
+    
+    public static boolean verificaGanhador2(int[][] array) {
+        
+        boolean resposta = false;
+        
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[0][0] + array[0][1] + array[0][2] == 3) {
+                    resposta = true;
+
+                } else if (array[1][0] + array[1][1] + array[1][2] == 3) {
+                    resposta = true;
+                } else if (array[2][0] + array[2][1] + array[2][2] == 3) {
+                    resposta = true;
+                } else if (array[0][0] + array[1][1] + array[2][2] == 3) {
+                    resposta = true;
+                } else if (array[0][2] + array[1][1] + array[2][0] == 3) {
+                    resposta = true;
+                }
+
+            }
+
+        }
+        return resposta;
+
     }
 
 }
