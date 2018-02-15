f80486d4866892c57b4bf378c0e1208a894476ab
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-02-15 20:21:22 -0300

    Update Exer04.java

--- Exer04.java
+++ Exer04.java
@@ -4,8 +4,6 @@ public class Exer04 {
     public static void main(String[] args) {
         JogoVelha jogoDaVelha = new JogoVelha();
         jogoDaVelha.inicioJogo();
-        jogoDaVelha.validacoes();
-        jogoDaVelha.verificarGanhador();
     }
     
 }
