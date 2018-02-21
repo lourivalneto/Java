3a6fa9512d11d5e84ec72c79e37cdf259fdaa0f5
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-02-21 11:59:31 -0400

    Exer02

---
+++ curso-java-basico/src/com/leandro/aula34/labs/Calculadora.java
@@ -0,0 +1,44 @@
+package com.leandro.aula34.labs;
+
+public class Calculadora {
+	private static int soma;
+	private static int subtracao;
+	private static int multiplicacao;
+	private static int divisao;
+	private static double potencia;
+	
+	
+	
+	public static int somar(int num1, int num2) {
+		soma = num1 + num2;
+		return soma;
+	}
+	
+	public static int subtrair(int num1, int num2) {
+		subtracao =  num1 - num2;
+		return subtracao;
+	}
+	
+	public static int multiplicar(int num1, int num2) {
+		multiplicacao = num1 * num2;
+		return multiplicacao;
+	}
+	
+	public static int dividir(int num1, int num2) {
+		divisao =  num1 / num2;
+		return divisao;
+	}
+	
+	public static double elevar(int num1, int num2) { // EX: 2 elevado a 3
+		
+		for(int i = 1; i < num2; i++) {
+			if(i == 1) {
+				potencia = num1; // potencia recebe 2, na primeira passagem // NÃO ENTRA MAIS
+			}
+			potencia *= num1; // ex: potencia recebe ele (2) vezes num1 = 2x2 = 4 // 4X2 = 8
+		}
+		return potencia;
+		
+	}
+
+}
