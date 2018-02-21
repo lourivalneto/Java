db1e77213a672ea6c11b1fba04cc78c9128fc213
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-02-20 20:42:18 -0400

    Aula 34.

---
+++ curso-java-basico/src/com/leandro/aula34/TesteCalculadora.java
@@ -0,0 +1,21 @@
+package com.leandro.aula34;
+
+public class TesteCalculadora {
+	static int resultSoma;
+
+	public static void main(String[] args) {
+		
+		//MinhaCalculadora calc = new MinhaCalculadora();
+		resultSoma = MinhaCalculadora.soma(1,2);
+		
+		soma2Valores(1, 2);
+		
+		System.out.println(resultSoma + " e "+ soma2Valores(1, 2));
+
+	}
+	
+	static int soma2Valores(int num1, int num2) {
+		return MinhaCalculadora.soma(num1, num2);
+	}
+
+}
