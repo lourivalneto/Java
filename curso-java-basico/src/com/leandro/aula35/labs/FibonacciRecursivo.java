3521a19bba980ddeec1a052aa979021a78761e19
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-02-24 10:38:18 -0400

    Exercício 1 da aula 35.

---
+++ curso-java-basico/src/com/leandro/aula35/labs/FibonacciRecursivo.java
@@ -0,0 +1,20 @@
+package com.leandro.aula35.labs;
+
+public class FibonacciRecursivo {
+	static int primeiroTermo = 0;
+	static int segundoTermo = 1;
+	static int valorTermo = 0;
+	static int cont = 3;
+
+	public static int fibonacciR(int nTermo) {
+
+		if (nTermo == 2 || nTermo == 3) {
+			return 1;
+		}
+
+		return fibonacciR(nTermo - 1) + fibonacciR(nTermo - 2);
+							//5  	  +					4
+
+	}
+
+}
