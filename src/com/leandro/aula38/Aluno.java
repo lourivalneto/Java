ee76704c4bbcc746bc670b4a1b1fb252e9989066
leandro <leandro@leandro-B85M-D3PH>
2018-02-28 17:10:24 -0400

    Exemplos da Aula 38.

---
+++ src/com/leandro/aula38/Aluno.java
@@ -0,0 +1,42 @@
+
+package com.leandro.aula38;
+
+public class Aluno extends Pessoa {
+    
+    private String curso;
+    private double[] notas;
+    
+    public Aluno(){
+        super();
+    }
+    
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.setNotas(notas);
+    }
+    
+    public double calcularMedia(){
+        return 0;
+    }
+    
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    
+    
+    
+    
+}
