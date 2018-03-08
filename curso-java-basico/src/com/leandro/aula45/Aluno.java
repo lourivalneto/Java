4390cb09af31a2ced40cc250deaa07d9813bccd1
leandro <leandro@computador-pessoal-do-leandro>
2018-03-07 21:58:54 -0400

    Aula 45, sobre castings e instanceof.

---
+++ curso-java-basico/src/com/leandro/aula45/Aluno.java
@@ -0,0 +1,64 @@
+//teste..
+package com.leandro.aula45;
+
+public class Aluno extends Pessoa {
+    
+    private String curso;
+    private double[] notas;
+    
+    public Aluno(){
+        super();
+    }
+    
+    
+    
+
+    public Aluno(String nome, String endereco, String telefone, String curso) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+		
+	}
+
+
+
+
+	public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.setNotas(notas);
+    }
+    
+    public double calcularMedia(){
+        return 0;
+    }
+    
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    public void metodoQualquer() {
+    	super.setCpf("343434343443");
+    	
+    	this.setCpf("31325446465");
+    }
+    
+    public String obterEtiquetaEndereco(){
+        String s = "Endereço do Aluno: ";
+        s+= super.getEndereco();
+        
+        return s;
+        //return endereco;
+    }
+    
+    
+}
