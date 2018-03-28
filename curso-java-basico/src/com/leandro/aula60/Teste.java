2e2aa24cbd269829469a22ef50740efce0487739
leandro <leandrocgms1986#outlook.com>
2018-03-28 19:00:10 -0400

    Aulas sobre escopo de variáveis, passagem por valor e referência e sobre Varargs.

---
+++ curso-java-basico/src/com/leandro/aula60/Teste.java
@@ -0,0 +1,24 @@
+package com.leandro.aula60;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		EscopoVariaveis escopo = new EscopoVariaveis();
+		escopo.setValor(10);
+		
+		System.out.println(escopo.getValor());
+		
+		System.out.println(escopo.calculaValor(20));
+		
+		System.out.println(escopo.getValor());
+		
+		System.out.println();
+		
+		System.out.println(escopo.teste());
+		
+		System.out.println(escopo.getValor());
+
+	}
+
+}
