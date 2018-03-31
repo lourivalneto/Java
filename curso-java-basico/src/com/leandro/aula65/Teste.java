a8b51b633b52a9de61228772d9bb0bf4eed0bbe4
leandro <leandrocgms1986#outlook.com>
2018-03-30 20:21:19 -0400

    Aulas sobre Annotations, GC e Threads.

---
+++ curso-java-basico/src/com/leandro/aula65/Teste.java
@@ -0,0 +1,21 @@
+package com.leandro.aula65;
+@InformacaoAula(
+		autor = "Loiane",
+		aulaNumero = 65,
+		blog = "loiane.com"
+		)
+
+public class Teste {
+	@InformacaoAula(
+			autor = "Loiane",
+			aulaNumero = 65,
+			blog = "loiane.com"
+			)
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
