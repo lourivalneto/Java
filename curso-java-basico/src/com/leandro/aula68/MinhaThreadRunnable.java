af3af4065f0f9891c9286784a6b1720671a96d92
leandro <leandro@leandro-B85M-D3PH>
2018-04-01 09:41:10 -0400

    Aula 69, sobre método join para esperar a execução de uma ou mais thr...

--- curso-java-basico/src/com/leandro/aula68/MinhaThreadRunnable.java
+++ curso-java-basico/src/com/leandro/aula68/MinhaThreadRunnable.java
@@ -10,8 +10,8 @@ public class MinhaThreadRunnable implements Runnable {
 	public MinhaThreadRunnable(String nome, int tempo) {
 		this.nome = nome;
 		this.tempo = tempo;
-		Thread t = new Thread(this);
-		t.start();
+		//Thread t = new Thread(this);
+		//t.start();
 	}
 
 	@Override
