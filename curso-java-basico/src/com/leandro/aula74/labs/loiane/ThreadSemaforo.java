ea396413bf3bf57867cceaaf395e3b84c5b06a25
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-03 17:54:45 -0400

    ok

---
+++ curso-java-basico/src/com/leandro/aula74/labs/loiane/ThreadSemaforo.java
@@ -0,0 +1,78 @@
+package com.leandro.aula74.labs.loiane;
+
+public class ThreadSemaforo implements Runnable {
+	private CorSemaforo cor;
+	private boolean parar;
+	private boolean corMudou;
+
+	public ThreadSemaforo() {
+		this.cor = CorSemaforo.VERMELHO;
+
+		this.parar = false;
+		this.corMudou = false;
+
+		new Thread(this).start();
+	}
+
+	@Override
+	public void run() {
+		while(!parar) {
+			try {
+				/*switch(this.cor) {
+				case VERMELHO:
+
+					Thread.sleep(2000);
+
+					break;
+				case AMARELO:
+					Thread.sleep(300);
+					break;
+				case VERDE:
+					Thread.sleep(1000);
+					break;
+				}*/
+				Thread.sleep(this.cor.getTempoEspera());
+				this.mudarCor();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private synchronized void mudarCor() {
+		switch(this.cor) {
+		case VERMELHO:
+			this.cor = CorSemaforo.VERDE;
+			break;
+		case AMARELO:
+			this.cor = CorSemaforo.VERMELHO;
+			break;
+		case VERDE:
+			this.cor = CorSemaforo.AMARELO;
+			break;
+		}
+		this.corMudou = true;
+		notify();
+	}
+	
+	public synchronized void esperaCorMudar() {
+		while(!corMudou) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		this.corMudou = false;
+	}
+	
+	public synchronized void desligarSemaforo() {
+		this.parar = true;
+	}
+
+	public CorSemaforo getCor() {
+		return cor;
+	}
+	
+	
+}
