fcde6a66157e67ccfb1301272aee119b94d9fb4c
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-05 21:34:16 -0400

    Ok.

---
+++ curso-java-basico/src/com/leandro/aula85_100/Aula85Math.java
@@ -0,0 +1,33 @@
+package com.leandro.aula85_100;
+
+import static java.lang.Math.*;
+
+public class Aula85Math {
+
+	public static void main(String[] args) {
+
+		System.out.println(pow(2, 3));
+
+		System.out.println(round(4.7));
+
+		System.out.println(round(4.4));
+
+		System.out.println(ceil(4.7));
+
+		System.out.println(ceil(4.4));
+		
+		System.out.println(floor(4.7));
+
+		System.out.println(floor(4.4));
+		
+		System.out.println(round(random() * 100));
+		
+		System.out.println(sin(toRadians(30)));
+		
+		System.out.println(cos(toRadians(1)));
+		
+		System.out.println(tan(toRadians(45)));
+
+	}
+
+}
---
+++ curso-java-basico/src/com/leandro/aula85_100/Aula86Date.java
@@ -0,0 +1,19 @@
+package com.leandro.aula85_100;
+
+import java.util.Date;
+
+public class Aula86Date {
+
+	public static void main(String[] args) {
+		
+		Date hoje = new Date();
+		
+		System.out.println(hoje);
+		
+		System.out.println("Milissegundos desde 1970 " + hoje.getTime());
+		
+		System.out.println(hoje.getDate());
+
+	}
+
+}
