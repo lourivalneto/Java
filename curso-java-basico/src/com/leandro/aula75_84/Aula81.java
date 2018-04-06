1be1d4fc58348789542cb397c29d4847da8ab498
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-05 20:25:09 -0400

    ok.

---
+++ curso-java-basico/src/com/leandro/aula75_84/Aula81.java
@@ -0,0 +1,16 @@
+package com.leandro.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+		
+		String teste = "Teste";
+		String testeMinusc = teste.toLowerCase();
+		String testeMaiusc = teste.toUpperCase();
+		
+		System.out.println(testeMinusc);
+		System.out.println(testeMaiusc);
+
+	}
+
+}
