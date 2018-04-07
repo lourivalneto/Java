adb25e9e73b88c85c64f091bcbd56601ab439b4f
leandro <leandro@leandro-B85M-D3PH>
2018-04-07 10:28:37 -0400

    ok.

---
+++ curso-java-basico/src/com/leandro/aula85_100/Aula91DateFormat.java
@@ -0,0 +1,58 @@
+package com.leandro.aula85_100;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Aula91DateFormat {
+
+	public static void main(String[] args) {
+		
+		Date hoje = new Date();
+		System.out.println(Locale.getDefault());
+		
+		//Locale.setDefault(new Locale("en","US"));
+		//Locale.setDefault(new Locale("pt","Brazil"));
+		
+		String hojeFormatado = DateFormat.getInstance().format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance().format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getDateInstance().format(hoje);
+		System.out.println(hojeFormatado); //dd//MMM/yyyy
+		
+		hojeFormatado = DateFormat.getDateTimeInstance().format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance(DateFormat.SHORT).format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance(DateFormat.LONG).format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getDateTimeInstance(
+		DateFormat.SHORT, DateFormat.SHORT).format(hoje);
+		
+		System.out.println(hojeFormatado);
+		
+		String data = "12/02/2017 14:25";
+		try {
+			Date dataDate = DateFormat.getInstance().parse(data);
+			System.out.println(dataDate);
+			
+			Calendar calendario = Calendar.getInstance();
+			calendario.setTime(dataDate);
+			System.out.println(calendario);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
