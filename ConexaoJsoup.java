cff66a365ee49ba8583dda47965a11810d625b5a
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-07-31 18:06:59 -0400

    Algumas Alterações
    Já não retorna erro no Debug, mas não carrega nada no EditText caixaTexto.

--- ConexaoJsoup.java
+++ ConexaoJsoup.java
@@ -9,30 +9,39 @@ import org.jsoup.select.Elements;
 import java.lang.ref.WeakReference;
 
 public class ConexaoJsoup extends AsyncTask<String,Integer,StringBuilder> {
-    private WeakReference<NomeDaActivity> activity;
+    //private WeakReference<ConversorMoedas> activity;
+    private ConversorMoedas conversorActivity;
+    private StringBuilder retornoConexao;
 
-    public ConexaoJsoup(NomeDaActivity activity) {
-        this.activity = new WeakReference<>(activity);
+    public ConexaoJsoup(ConversorMoedas conversorMoedas) {
+        this.conversorActivity = conversorMoedas;
     }
 
     @Override
     protected StringBuilder doInBackground(String... strings) {
-        StringBuilder retornoConexao = new StringBuilder();
+        this.retornoConexao = new StringBuilder();
         try {
-            Document melhorcambio = Jsoup.connect("https://www.melhorcambio.com/dolar-hoje").get();
-            Elements dolarEmReal = melhorcambio.select("input#comercial");
+//            Document melhorcambio = Jsoup.connect("https://www.melhorcambio.com/dolar-hoje").get();
+//            Elements dolarEmReal = melhorcambio.select("input#taxa-comercial");
 
             Document googleCambio = Jsoup.connect("https://www.google.com/search?q=d%C3%B3lar+turismo+hoje&ie=utf-8&oe=utf-8&client=firefox-b-ab").get();
             Elements tdGoogleCambios = googleCambio.select("tr.td");
-            retornoConexao.append(tdGoogleCambios.text()); // Quase certo que pega só os textos
+            //retornoConexao.append(tdGoogleCambios.text()); // Quase certo que pega só os textos
             // dentro das tags td
-//            for(int i = 0; i < tdGoogleCambios.size(); i++) {
-//
-//            }
+            retornoConexao.append(tdGoogleCambios.text()); // Quase certo que pega só os textos
         } catch (Exception e) {
             System.out.println("Segue erro abaixo: ");
             e.printStackTrace();
         }
+        this.retornoConexao = retornoConexao;
         return retornoConexao;
+    }
+
+    @Override
+    protected void onPostExecute(StringBuilder stringBuilder) {
+        //conversorActivity.setRetornoConexao(this.retornoConexao);
+        conversorActivity.caixaTexto.setText(stringBuilder);
+        super.onPostExecute(stringBuilder);
+
     }
 }
