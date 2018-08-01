1ce9dc0b32c19c437414a8811eec8d0a2e7ab4a4
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-08-01 08:30:53 -0400

    Consegui
    Usando recursividade.

--- Activity.java
+++ Activity.java
@@ -13,7 +13,17 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class NomeDaActivity extends AppCompatActivity {
-    private EditText caixaTexto;
+    EditText caixaTexto;
+    private StringBuilder retornoConexao;
+    private Handler handler = new Handler();
+    ConexaoThread conexaoT;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        verificarTerminoConexao();
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,31 @@ public class NomeDaActivity extends AppCompatActivity {
         setContentView(R.layout.nome_da_activity);
         setTitle("Título da Activity");
         caixaTexto = findViewById(R.id.caixaTexto);
-        ConexaoJsoup conexao = new ConexaoJsoup(this);
+        //        ConexaoJsoup conexao = new ConexaoJsoup(this);
+//        conexao.doInBackground();
+        conexaoT = new ConexaoThread(this);
+    }
+
+    void receberStringBuilder(StringBuilder retorno) {
+        this.retornoConexao = retorno;
+
+    }
+
+    private void verificarTerminoConexao() {
+        if (conexaoT.isAlive()) {
+            verificarTerminoConexao();
+        } else {
+            if (retornoConexao != null) {
+                caixaTexto.setText(retornoConexao);
+            } else {
+                caixaTexto.setText("Não foi possível estabelecer conexão com o servidor.");
+            }
+        }
+    }
+
+
+}
+
 
     }
 }
